849b89d5efa47b05cab00b0a99f18f6d0712cadd
osaslover <osaslover31542@gmail.com>
2026-06-21 21:26:58 +0100

    Set up Express server for Forex bot

--- src/app.js
+++ src/app.js
@@ -1,5 +1,17 @@
-console.log("Bot started");
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Forex bot is running");
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
 
 setInterval(() => {
-  console.log("Bot running...");
+  console.log("Scanning market...");
 }, 60000);
