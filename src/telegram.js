ef005ef444bbd0d0619920c55e386916a7f97118
osaslover <osaslover31542@gmail.com>
2026-06-21 21:46:34 +0100

    Update sendSignal function to send messages via bot

--- src/telegram.js
+++ src/telegram.js
@@ -1,5 +1,12 @@
+const TelegramBot = require("node-telegram-bot-api");
+
+const token = process.env.TELEGRAM_TOKEN;
+const chatId = process.env.CHAT_ID;
+
+const bot = new TelegramBot(token);
+
 async function sendSignal(message) {
-  console.log(message);
+  await bot.sendMessage(chatId, message);
 }
 
 module.exports = { sendSignal };
