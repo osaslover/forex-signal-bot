fdaff3c40819e6301c395a9a33aa85ca8510869d
osaslover <osaslover31542@gmail.com>
2026-06-21 22:20:59 +0100

    Update telegram.js

--- src/telegram.js
+++ src/telegram.js
@@ -1,17 +1,17 @@
-const TelegramBot = require("node-telegram-bot-api/src/telegram");
+const TelegramBot = require("node-telegram-bot-api");
 
-const token = process.env.TELEGRAM_TOKEN;
-const chatId = process.env.CHAT_ID;
+const token = "8667680010:AAFwV5aSirDMZhQsOGtkzii1hZ3lDnoQpjk";
+const chatId = "5929858944";
 
 const bot = new TelegramBot(token);
 
 async function sendSignal(message) {
   try {
     await bot.sendMessage(chatId, message);
-    console.log("Telegram message sent");
+    console.log("Message sent");
   } catch (error) {
-    console.log("Telegram error:", error.message);
+    console.error(error);
   }
 }
 
-module.exports = { sendSignal };
+sendSignal("🚀 Test message from Render");
