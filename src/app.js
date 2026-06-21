21220686d876d42bbadeb20c4916a040c3e5aa2e
osaslover <osaslover31542@gmail.com>
2026-06-21 21:34:14 +0100

    Remove Express server and related code
    Removed Express server setup and logging.

--- src/app.js
+++ src/app.js
@@ -1,16 +1,4 @@
-const express = require("express");
-
-const app = express();
-
-app.get("/", (req, res) => {
-  res.send("Forex bot is running");
-});
-
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+console.log("Forex Signal Bot Started");
 
 setInterval(() => {
   console.log("Scanning market...");
