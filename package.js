5861b70cd3e5bb79c54c99d446a29af687db719c
osaslover <osaslover31542@gmail.com>
2026-09-12 10:05:38 +0100

    Create package.js

---
+++ package.js
@@ -0,0 +1,14 @@
+{
+  "name": "smart-trading-bot",
+  "version": "1.0.0",
+  "description": "Multi-timeframe market analysis and Telegram signal bot",
+  "main": "src/app.js",
+  "scripts": {
+    "start": "node src/app.js"
+  },
+  "dependencies": {
+    "dotenv": "^16.4.5",
+    "express": "^4.21.2",
+    "node-telegram-bot-api": "^0.66.0"
+  }
+}
